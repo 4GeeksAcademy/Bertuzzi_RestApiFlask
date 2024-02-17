7f61cb21b005f1febe0f4b2b63f7a4eb56c9c203
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-02-17 17:05:53 +0000

    update codespace

--- src/app.py
+++ src/app.py
@@ -3,29 +3,28 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
         "hello": "world",
@@ -35,7 +34,6 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# Add family member (completed)
 
 @app.route('/members', methods=['POST'])
 def new_member():
@@ -50,7 +48,6 @@ def new_member():
         }
         return response_body,200
 
-# Fetch family member (completed)
 
 @app.route('/member/<int:member_id>', methods = ['GET'])
 def fetch_member(member_id):
@@ -64,7 +61,6 @@ def fetch_member(member_id):
     else:
         return 'Member not found', 404
     
-# Delete family member (completed)
 
 @app.route('/member/<int:member_id>', methods = ['DELETE'])
 def delete_member(member_id):
@@ -78,7 +74,7 @@ def delete_member(member_id):
     else:
         return 'Member not found', 404
 
-# this only runs if `$ python src/app.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -28,7 +29,6 @@ class FamilyStructure:
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
-
     def add_member(self, member):
         if 'id' in member:
                 member["last_name"] = self.last_name
@@ -38,18 +38,15 @@ class FamilyStructure:
                 member['id'] = self._generateId()
                 self._members.append(member)
                 return member
-    
     def get_member(self, id):
         for member in self._members:
             if member["id"] == id:
                 return member
-
     def delete_member(self, id):
         for member in self._members:
             if member["id"] == id:
                 index = self._members.index(member)
                 self._members.pop(index)
                 return self._members
-
     def get_all_members(self):
         return self._members
